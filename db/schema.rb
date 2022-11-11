573e4b486440f610dbaacc19a051afb60ea6b102
James White <whitejaad@gmail.com>
2022-11-11 12:03:07 -0800

    added link to each bulk discount

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_11_031721) do
+ActiveRecord::Schema.define(version: 2022_11_11_031558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bulk_discounts", force: :cascade do |t|
-    t.float "percent_discount"
+    t.integer "percent_discount"
     t.integer "quantity_threshold"
+    t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "merchant_id"
     t.index ["merchant_id"], name: "index_bulk_discounts_on_merchant_id"
   end
 
