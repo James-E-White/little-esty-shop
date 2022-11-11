a3792ba955e7f3e942e4e614e6baab03625d1869
James White <whitejaad@gmail.com>
2022-11-11 09:36:21 -0800

    updates US to be on the correct page.

--- config/routes.rb
+++ config/routes.rb
@@ -12,13 +12,13 @@ Rails.application.routes.draw do
 
   # merchants
   get '/merchants/:merchant_id/dashboard', to: 'merchants#show'
-  get '/merchants/:merchant_id/bulk_discounts', to: 'merchants#show'
+  get '/merchants/:merchant_id/dashboard/bulk_discounts', to: 'bulk_discounts#index'
   patch 'merchants/:merchant_id/items/:id', to: 'items#update'
 
   resources :merchants, only: [] do
     resources :items, only: %i[index show edit new create]
     resources :item_status, only: [:update]
     resources :invoices, only: %i[index show update]
-    # resources :bulk_discounts, only: [:show]
+    resources :bulk_discounts, only: %i[index show]
   end
 end
